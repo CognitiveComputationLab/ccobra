25bd11e86d1f2dcac1129e93258bb7a7e113c512
Nicolas Riesterer <riestern@tf.uni-freiburg.de>
2018-08-09 15:25:29 +0200

    Extend setup.py (PyPI compliance)
    - Update .gitignore to ignore build/dist files
    - Add organization to LICENSE
    - Update setup.py to include additional information (PyPI)

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='ORCA',
     description='Core functionality and class interfaces of ORCA.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     version='0.1',
     packages=['orca'],
     license='MIT',
     author='Nicolas Riesterer, Daniel Brand, Lukas Elflein',
-    author_email='riestern@tf.uni-freiburg.de, daniel.brand@cognition.uni-freiburg.de, elfleinl@tf.uni-freiburg.de'
+    author_email='riestern@tf.uni-freiburg.de, daniel.brand@cognition.uni-freiburg.de, elfleinl@tf.uni-freiburg.de',
+    install_requires=[
+        'numpy',
+        'pandas'
+    ],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    )
 )
