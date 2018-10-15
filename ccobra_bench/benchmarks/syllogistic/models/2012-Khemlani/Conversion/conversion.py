b9e253ddfe48c1702712c713d7085d4ae180e304
Nicolas Riesterer <riestern@cs.uni-freiburg.de>
2018-10-15 16:47:26 +0200

    Add Khemlani models to benchmark
    - Add Khemlani 2012 predictions
    - Update benchmarks

---
+++ ccobra_bench/benchmarks/syllogistic/models/2012-Khemlani/Conversion/conversion.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+import ccobra
+
+class Conversion(ccobra.CCobraModel):
+    def __init__(self, name='Conversion'):
+        super(Conversion, self).__init__(
+            name, ['syllogistic'], ['single-choice'])
+
+        pred_df = pd.read_csv('Conversion.csv')
+        self.predictions = dict(
+            zip(
+                pred_df['Syllogism'].tolist(),
+                [x.split(';') for x in pred_df['Prediction']]))
+
+    def predict(self, item, **kwargs):
+        enc_task = ccobra.syllogistic.encode_task(
+            '/'.join([';'.join(x) for x in item.task]))
+        enc_resp = self.predictions[enc_task]
+        dec_resp = [ccobra.syllogistic.decode_response(x, item.task) for x in enc_resp]
+        return dec_resp[np.random.randint(0, len(dec_resp))]
