fa6e8aa8d0b8cde486b0c6f108d87d7c3e64a40b
Daniel Brand <daniel.brand@metech.tu-chemnitz.de>
2025-07-15 16:01:39 +0200

    Updated setup.py to no longer contain the information now provided in pyproject.toml

--- setup.py
+++ setup.py
@@ -1,34 +1,4 @@
 import setuptools
 
-import ccobra
-
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-
 setuptools.setup(
-    name='ccobra',
-    description='The CCOBRA framework.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/CognitiveComputationLab/ccobra',
-    version=ccobra.__version__,
-    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
-    license='MIT',
-    author='Nicolas Riesterer',
-    author_email='riestern@tf.uni-freiburg.de',
-    install_requires=[
-        'numpy',
-        'pandas'
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent'
-    ],
-    entry_points={
-        'console_scripts': ['ccobra=ccobra.benchmark.runner:entry_point']
-    },
-    package_data={
-        'ccobra.benchmark.visualization': ['*.js', '*.html', '*.css']
-    }
 )
